1cddf963b1f24907a8e79b8b3d253e149cdee2c4
Jonas <ermert.jonas@yahoo.de>
2021-11-15 01:29:43 +0100

    Implemented the logic

--- src/com/jonasermert/Main.java
+++ src/com/jonasermert/Main.java
@@ -3,6 +3,16 @@ package com.jonasermert;
 public class Main {
 
     public static void main(String[] args) {
-	    
+
+        int n = 2004;
+
+        if(n%4 == 0) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("No Leap Year");
+        }
+
+
+
     }
 }
